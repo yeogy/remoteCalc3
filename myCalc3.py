30e2f79f7d4e40adc9f3141107c3ca15c14b4ae9
Win <tenacity159@gmail.com>
2022-10-20 11:38:26 +0900

    master1

---
+++ myCalc3.py
@@ -0,0 +1,13 @@
+from add import add_func
+from sub import mul_func
+##함수 선언부
+
+##전역 변수부
+num1, num, res = 100, 200, 0
+
+##메인 코드부
+res = add_func(num1, num2)
+print(num1, "+", num2, "=", res)
+
+res = sub_func(num1, num2)
+print(num1, "-", num2, "=", res)
